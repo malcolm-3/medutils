69691b76cd1131c2b86211c1ee607870b50c9b7d
Malcolm <mnjjunk@comcast.net>
2025-03-20 17:25:32 -0400

    dependency version updates

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import find_packages, setup  # type: ignore   ## required by github
+from setuptools import find_packages, setup  # # type: ignore   ## required by github
 
 setup(
     name="medutils",
